e579d718bf65c1ea61095735e56398ba80b8e963
ValeDev <valedev@MacBook-Air-de-ValeDev.local>
2026-06-19 23:25:05 -0600

    reorder personal links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,10 +33,10 @@ const profile = {
   title: "Valentín Martínez",
   banner: "Technology Developer for Social & Environmental Impact",
   links: [
-    { title: "Blog Literario", url: "https://valentinmartinezmx.wixsite.com/ideas/blog" },
     { title: "GitHub", url: "https://github.com/ValenteCreativo" },
-    { title: "Blog Técnico", url: "https://medium.com/@geovalente" },
     { title: "Programación Creativa", url: "https://codepen.io/ValenteCreativo" },
+    { title: "Blog Literario", url: "https://valentinmartinezmx.wixsite.com/ideas/blog" },
+    { title: "Blog Técnico", url: "https://medium.com/@geovalente" },
     { title: "TA'AK Studio", url: "https://taak-studio.bubbleapps.io/version-test" },
   ],
   hackathons: [
